6ec503ac52be27f8685b56bb2860615cfceb250f
mohgithub27 <112115876+mohgithub27@users.noreply.github.com>
2023-04-26 11:09:25 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,15 @@
-resource "aws_s3_bucket" "Iac_demo_bucket" {
-  bucket = "Iac-demo-bucket"
+resource "aws_s3_bucket" "Source_Iac_demo_bucket" {
+  bucket = "Source_Iac-demo-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "Demo Bucket for POC"
+    Environment = "Non-Prod"
+  }
+}
+
+resource "aws_s3_bucket" "Destination_Iac_demo_bucket" {
+  bucket = "Destination_Iac-demo-bucket"
   acl    = "private"
 
   tags = {
