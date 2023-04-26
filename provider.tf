de1d20de1af73dccdfd08ec2c140e7f8d503e676
mohgithub27 <112115876+mohgithub27@users.noreply.github.com>
2023-04-26 09:46:33 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIARYORTJIQSMB5IYMI"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
